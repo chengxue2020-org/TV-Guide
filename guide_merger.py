d76a2d5ad933c3018b7f42c3c6f2ca2301571357
chengxue2020 <65702849+chengxue2020@users.noreply.github.com>
2026-05-11 22:56:14 +0800

    尝试解决源EPG下载失败问题

--- guide_merger.py
+++ guide_merger.py
@@ -679,7 +679,7 @@ def download_file(url: str, path: str) -> Optional[str]:
     headers = {
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8',
         'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
-        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Encoding': 'gzip, deflate',
         'Connection': 'keep-alive',
         'Upgrade-Insecure-Requests': '1',
         'Sec-Fetch-Dest': 'document',
